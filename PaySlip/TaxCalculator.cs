cb5f066d038e4c10f6ef185bdb8f23c84442828b
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-06-25 10:03:16 +1200

    tests and funcs for calculating gross income and super

--- PaySlip/TaxCalculator.cs
+++ PaySlip/TaxCalculator.cs
@@ -1,7 +1,23 @@
-﻿namespace PaySlip
+﻿using System;
+
+namespace PaySlip
 {
     public class TaxCalculator
     {
-        
+        public int CalculateGrossIncome(int annualSalary) // monthsOfPaymentPeriod
+        {
+            var monthsInAYear = 12;
+            var grossIncome = annualSalary / monthsInAYear; // * monthsOfPaymentPeriod;
+
+            return grossIncome;
+        }
+
+        public double CalculateSuper(int grossIncome, int superRate)
+        {
+            var superRateInDecimal = (double) superRate / 100;
+            var expectedSuper = grossIncome * superRateInDecimal;
+
+            return Math.Floor(expectedSuper);
+        }
     }
 }
--- PaySlipTest/TaxCalculatorShould.cs
+++ PaySlipTest/TaxCalculatorShould.cs
@@ -1,7 +1,34 @@
-﻿namespace PaySlipTest
+﻿using System;
+using PaySlip;
+using Xunit;
+
+namespace PaySlipTest
 {
     public class TaxCalculatorShould
     {
-        
+        [Theory]
+        [InlineData(60050, 12, 5004)]
+        [InlineData(70000, 12, 5833)]
+        [InlineData(80000, 12, 6666)]
+        public void ReturnGrossIncome(int annualSalary, int monthsInAYear, int actualGrossIncome)
+        {
+            var taxCalculator = new TaxCalculator();
+            var expectedGrossIncome = taxCalculator.CalculateGrossIncome(annualSalary);
+            
+            Assert.Equal(expectedGrossIncome, actualGrossIncome);
+        }
+
+        [Theory]
+        [InlineData(5004, 9, 450)]
+        [InlineData(5833, 9, 524)]
+        [InlineData(6666, 9, 599)]
+        public void ReturnSuper(int grossIncome, int superRate, int actualSuper)
+        {
+            var taxCalculator = new TaxCalculator();
+            var expectedSuper = taxCalculator.CalculateSuper(grossIncome, superRate);
+
+            Assert.Equal(expectedSuper, actualSuper);
+        }
+
     }
 }
